432257479c9e85284f0fadc3efc4cb026526153d
junchoon14 <40448406+junchoon14@users.noreply.github.com>
2024-02-05 17:38:24 +0800

    update 2nd menu

--- script.js
+++ script.js
@@ -1,35 +1,40 @@
 document.addEventListener("DOMContentLoaded", function(){
-  // make it as accordion for smaller screens
-  if (window.innerWidth < 992) {
-  
-    // close all inner dropdowns when parent is closed
-    document.querySelectorAll('.navbar .dropdown').forEach(function(everydropdown){
-      everydropdown.addEventListener('hidden.bs.dropdown', function () {
-        // after dropdown is hidden, then find all submenus
-          this.querySelectorAll('.submenu').forEach(function(everysubmenu){
-            // hide every submenu as well
-            everysubmenu.style.display = 'none';
-          });
-      })
-    });
-  
-    document.querySelectorAll('.dropdown-menu a').forEach(function(element){
-      element.addEventListener('click', function (e) {
-          let nextEl = this.nextElementSibling;
-          if(nextEl && nextEl.classList.contains('submenu')) {	
-            // prevent opening link if link needs to open dropdown
-            e.preventDefault();
-            if(nextEl.style.display == 'block'){
-              nextEl.style.display = 'none';
-            } else {
-              nextEl.style.display = 'block';
-            }
-  
-          }
-      });
+// make it as accordion for smaller screens
+if (window.innerWidth < 992) {
+
+  // close all inner dropdowns when parent is closed
+  document.querySelectorAll('.navbar .dropdown').forEach(function(everydropdown){
+    everydropdown.addEventListener('hidden.bs.dropdown', function () {
+      // after dropdown is hidden, then find all submenus
+        this.querySelectorAll('.submenu').forEach(function(everysubmenu){
+          // hide every submenu as well
+          everysubmenu.style.display = 'none';
+          console.log('12345')
+        });
     })
-  }
-  // end if innerWidth
-  }); 
-  // DOMContentLoaded  end
+  });
+
+  document.querySelectorAll('.dropdown-menu a').forEach(function(element){
+    element.addEventListener('click', function (e) {
+        const nextEl = this.nextElementSibling;
+        const parentEl = this.parentElement.parentElement;
+        console.log('nextEl:' + nextEl.classList+', parentEl:'+ parentEl.classList)
+        if(nextEl && nextEl.classList.contains('submenu')) {
+          console.log('123')
+          e.preventDefault();
+          if(nextEl.style.display === 'none'){
+            nextEl.style.display = 'block';
+            parentEl.style.display = 'block';
+          } else {
+            nextEl.style.display = 'none';
+            console.log('1234')
+          }
+
+        }
+    });
+  })
+}
+// end if innerWidth
+}); 
+// DOMContentLoaded  end
 

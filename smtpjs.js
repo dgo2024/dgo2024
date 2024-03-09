3a68e77565a285ab144360116490f602ca3928db
junchoon14 <40448406+junchoon14@users.noreply.github.com>
2024-03-09 12:40:50 +0800

    Add feature of reply

--- smtpjs.js
+++ smtpjs.js
@@ -16,70 +16,78 @@ document.getElementById('dgo-travel-grant-form').addEventListener('submit', func
   const expected_expense = document.getElementById('expected-expense').value;
   const why_travel_grant = document.getElementById('why-travel-grant').value;
 
-  
-  let body = 'Hello,' + '<br><br><br>'; //%0A是換行 換了三行
-  body += ' <table>';
-  body += '  <tr>';
-  body += '   <td><strong>Name of applicant</strong></td>';
-  body += '   <td>' + name_of_applicant + '</td>';
-  body += '  </tr>';
+  const head = 'Hello,<br><br><br>';
+  const footer = `
+  <br><br><br>Best regards<br><br>
+  dg.o2024`;
+  let data = ' <table>';
+  data += '  <tr>';
+  data += '   <td><strong>Name of applicant</strong></td>';
+  data += '   <td>' + name_of_applicant + '</td>';
+  data += '  </tr>';
 
-  body += '  <tr>';
-  body += '   <td><strong>Institution</strong></td>';
-  body += '   <td>' + institution + '</td>';
-  body += '  </tr>';
+  data += '  <tr>';
+  data += '   <td><strong>Institution</strong></td>';
+  data += '   <td>' + institution + '</td>';
+  data += '  </tr>';
 
-  body += '  <tr>';
-  body += '   <td><strong>Phone Number</strong></td>';
-  body += '   <td>' + phone_number + '</td>';
-  body += '  </tr>';
+  data += '  <tr>';
+  data += '   <td><strong>Phone Number</strong></td>';
+  data += '   <td>' + phone_number + '</td>';
+  data += '  </tr>';
 
-  body += '  <tr>';
-  body += '   <td><strong>Address</strong></td>';
-  body += '   <td>' + address + '</td>';
-  body += '  </tr>';
+  data += '  <tr>';
+  data += '   <td><strong>Address</strong></td>';
+  data += '   <td>' + address + '</td>';
+  data += '  </tr>';
 
-  body += '  <tr>';
-  body += '   <td><strong>E-mail</strong></td>';
-  body += '   <td>' + email + '</td>';
-  body += '  </tr>';
+  data += '  <tr>';
+  data += '   <td><strong>E-mail</strong></td>';
+  data += '   <td>' + email + '</td>';
+  data += '  </tr>';
 
-  body += '  <tr>';
-  body += '   <td><strong>Personal Website</strong></td>';
-  body += '   <td>' + personal_website + '</td>';
-  body += '  </tr>';
+  data += '  <tr>';
+  data += '   <td><strong>Personal Website</strong></td>';
+  data += '   <td>' + personal_website + '</td>';
+  data += '  </tr>';
 
-  body += '  <tr>';
-  body += '   <td><strong>Title of Paper Accepted</strong></td>';
-  body += '   <td>' + title_of_paper_accepted + '</td>';
-  body += '  </tr>';
+  data += '  <tr>';
+  data += '   <td><strong>Title of Paper Accepted</strong></td>';
+  data += '   <td>' + title_of_paper_accepted + '</td>';
+  data += '  </tr>';
 
-  body += '  <tr>';
-  body += '   <td><strong>Abstract of Paper Accepted</strong></td>';
-  body += '   <td>' + abstract_of_paper_accepted + '</td>';
-  body += '  </tr>';
+  data += '  <tr>';
+  data += '   <td><strong>Abstract of Paper Accepted</strong></td>';
+  data += '   <td>' + abstract_of_paper_accepted + '</td>';
+  data += '  </tr>';
 
-  body += '  <tr>';
-  body += '   <td><strong>Departure Date</strong></td>';
-  body += '   <td>' + departure_date + '</td>';
-  body += '  </tr>';
+  data += '  <tr>';
+  data += '   <td><strong>Departure Date</strong></td>';
+  data += '   <td>' + departure_date + '</td>';
+  data += '  </tr>';
 
-  body += '  <tr>';
-  body += '   <td><strong>Return Date</strong></td>';
-  body += '   <td>' + return_date + '</td>';
-  body += '  </tr>';
+  data += '  <tr>';
+  data += '   <td><strong>Return Date</strong></td>';
+  data += '   <td>' + return_date + '</td>';
+  data += '  </tr>';
 
-  body += '  <tr>';
-  body += '   <td><strong>Expected expense</strong></td>';
-  body += '   <td>' + expected_expense + '</td>';
-  body += '  </tr>';
+  data += '  <tr>';
+  data += '   <td><strong>Expected expense</strong></td>';
+  data += '   <td>' + expected_expense + '</td>';
+  data += '  </tr>';
 
-  body += '  <tr>';
-  body += '   <td><strong>Why applying for travel grant</strong></td>';
-  body += '   <td>' + why_travel_grant + '</td>';
-  body += '  </tr>';
-  6
+  data += '  <tr>';
+  data += '   <td><strong>Why are you applying for this travel grant</strong></td>';
+  data += '   <td>' + why_travel_grant + '</td>';
+  data += '  </tr>';
+  data += ' </table>';
 
+  const replyInfo = `
+  Hello ${name_of_applicant},<br><br><br>
+  We have received your application.
+  <br><br>
+  ${data}
+  `;
 
   Email.send({
     Host: 'smtp.elasticemail.com',
@@ -87,13 +95,22 @@ document.getElementById('dgo-travel-grant-form').addEventListener('submit', func
     Password: '677813FEAE22572C88CC71BB0B2990576EA2',
     To: 'phauser@albany.edu',
     From: 'dgo2024.ntu@gmail.com',
-    Subject: 'dg.o2024 - Appliying for travel grant from' + name_of_applicant,
-    Body: body,
+    Subject: 'dg.o2024 - Application of travel grants from ' + name_of_applicant,
+    Body: head + data + footer,
   }).then((message) => {
     if (message === 'OK') {
       alert('Your application has been send successfully!');
       init();
       btn.value = 'Send';
+      Email.send({
+        Host: 'smtp.elasticemail.com',
+        Username: 'dgo2024.ntu@gmail.com',
+        Password: '677813FEAE22572C88CC71BB0B2990576EA2',
+        To: email,
+        From: 'dgo2024.ntu@gmail.com',
+        Subject: 'dg.o2024 - ' + name_of_applicant + ', your application has been accepted',
+        Body: replyInfo + footer,
+      });
     } else {
       btn.value = 'Send';
       alert('Some error has occurred! Please try again');
